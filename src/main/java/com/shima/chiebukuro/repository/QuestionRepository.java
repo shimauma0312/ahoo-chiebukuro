d4c28f6cce86fd62e3708a76cf778b3438f4f35f
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-10 05:17:54 +0900

    feat: Add insertQuestion method to QuestionRepository

--- src/main/java/com/shima/chiebukuro/repository/QuestionRepository.java
+++ src/main/java/com/shima/chiebukuro/repository/QuestionRepository.java
@@ -2,9 +2,12 @@ package com.shima.chiebukuro.repository;
 
 import java.util.List;
 
+import com.shima.chiebukuro.QuestionForm;
 import com.shima.chiebukuro.model.Question;
 
 public interface QuestionRepository {
+
+    void insertQuestion(QuestionForm questionForm);
 
     List<Question> findAll();
 
