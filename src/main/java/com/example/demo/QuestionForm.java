22a5e680f3f9d2fd83d2706d166a998e4640aedc
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-10 00:51:39 +0900

    chore: Add QuestionForm class for handling question form data

---
+++ src/main/java/com/example/demo/QuestionForm.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+public class QuestionForm {
+
+    @NotEmpty(message = "題名を入力してください")
+    @Pattern(regexp = ".{1,18}", message = "題名は18文字以内で入力してください")
+    private String title;
+
+    @NotEmpty(message = "質問内容を入力してください")
+    @Pattern(regexp = ".{1,255}", message = "質問内容は255文字以内で入力してください")
+    private String question;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
