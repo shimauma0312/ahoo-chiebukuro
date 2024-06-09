b97020107a6eec1d55b5b3e99d95a05cdbf01ce9
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-10 05:17:33 +0900

    feat: Add SQLite database setup in AhooChiebukuroApplication

--- src/main/java/com/shima/chiebukuro/AhooChiebukuroApplication.java
+++ src/main/java/com/shima/chiebukuro/AhooChiebukuroApplication.java
@@ -3,10 +3,13 @@ package com.shima.chiebukuro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.shima.chiebukuro.model.SQLiteDBSetup;
+
 @SpringBootApplication
 public class AhooChiebukuroApplication {
 
 	public static void main(String[] args) {
+		SQLiteDBSetup.setup();
 		SpringApplication.run(AhooChiebukuroApplication.class, args);
 	}
 
