e64fcf089d93f916a94e3325a90b6259e94d8aa3
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-10 05:17:41 +0900

    feat: Refactor SQLiteDBOperations to use prepared statements

---
+++ src/main/java/com/shima/chiebukuro/model/SQLiteDBOperations.java
@@ -0,0 +1,49 @@
+package com.shima.chiebukuro.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SQLiteDBOperations {
+
+    public void insert(String title, String question) {
+        String sql = "INSERT INTO questions(title, content) VALUES(?, ?)";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:app.db");
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, title);
+            pstmt.setString(2, question);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Question> selectQuestions() {
+        String sql = "SELECT title, content FROM questions";
+        List<Question> questions = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:app.db");
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                questions.add(new Question(title, content));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return questions;
+    }
+}
