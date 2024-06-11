403f034d7bffd495f08335150bca53237248844b
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-11 15:06:45 +0900

    feat: Add createdTime field to Question model

--- src/main/java/com/shima/chiebukuro/model/Question.java
+++ src/main/java/com/shima/chiebukuro/model/Question.java
@@ -3,10 +3,12 @@ package com.shima.chiebukuro.model;
 public class Question {
     private String title;
     private String content;
+    private String createdTime;
 
-    public Question(String title, String content) {
+    public Question(String title, String content, String createdTime) {
         this.title = title;
         this.content = content;
+        this.createdTime = createdTime;
     }
 
     public String getTitle() {
@@ -17,12 +19,20 @@ public class Question {
         return content;
     }
 
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
     }
 
 }
