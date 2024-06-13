2e754f2e6132f5d95ce38da6a22af71f91b35663
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-06-14 00:44:02 +0900

    refactor: Add id field to Question class in model package

--- src/main/java/com/shima/chiebukuro/model/Question.java
+++ src/main/java/com/shima/chiebukuro/model/Question.java
@@ -1,14 +1,20 @@
 package com.shima.chiebukuro.model;
 
 public class Question {
+    private int id;
     private String title;
     private String content;
     private String createdTime;
 
-    public Question(String title, String content, String createdTime) {
+    public Question(int id, String title, String content, String createdTime) {
+        this.id = id;
         this.title = title;
         this.content = content;
         this.createdTime = createdTime;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -21,6 +27,10 @@ public class Question {
 
     public String getCreatedTime() {
         return createdTime;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setTitle(String title) {
